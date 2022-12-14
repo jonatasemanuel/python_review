24049b8078c25e8241e7d1d769fcaee372e7e0d2
Jonatas Silva [WSL2] <jonatassilvaemanuel@gmail.com>
2022-12-14 20:07:31 -0300

    packages

--- class_review/car.py
+++ class_review/car.py
@@ -21,8 +21,32 @@ class Car():
             self.odometer_reading += mileage
         else:
             print("You can't roll back an odometer!")
-        
-        
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year) -> None:
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+class Battery():
+    def __init__(self, battery_size=70) -> None:
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        print(f'This car has a {self.battery_size} -kWh battery.')
+
+    def get_range(self):
+        if self.battery_size == 70: range = 240
+        elif self.battery_size == 85: range = 270
+        message = f'This car go approximately {range} miles on a full charge.'
+        print(message)
+    
+    def upgrade_battery(self):
+        if self.battery_size != 85:
+            self.battery_size = 85
+
+
 if __name__ == '__main__':
     my_new_car = Car('ford', 'eco- sport', 2018)
     print(my_new_car.get_descriptive_name())
---
+++ class_review/electric_car.py
@@ -0,0 +1,10 @@
+from car import ElectricCar
+
+
+if __name__ == '__main__':
+    my_tesla = ElectricCar('tesla', 'model s', 2016)
+    print(my_tesla.get_descriptive_name()) 
+    my_tesla.battery.describe_battery()
+    my_tesla.battery.get_range()
+    my_tesla.battery.upgrade_battery()
+    my_tesla.battery.get_range()
